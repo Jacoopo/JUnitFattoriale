dbdd19c19790868fd628842eb2a397c44fcd456f
Jacopo <Jacopo@tim.it>
2022-04-12 13:49:33 +0200

    versione 1.0

---
+++ src/Fattoriale.java
@@ -0,0 +1,15 @@
+
+public class Fattoriale {
+	public Fattoriale() {
+	}
+
+	public float fattoriale(int n) {
+		int fatt, i;
+		fatt = 1;
+		for (i = 1; i < n; i++) {
+			fatt = fatt * i;
+		}
+		return fatt;
+	}
+
+}
