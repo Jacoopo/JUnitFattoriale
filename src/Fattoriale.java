9b20b25a5346e7d2badc5c072f120601920941fe
4a <4a@pianodue11>
2022-04-12 13:53:19 +0200

    versione 2.0

--- src/Fattoriale.java
+++ src/Fattoriale.java
@@ -1,12 +1,20 @@
-
+/**
+ * 
+ * @author Canoci Jacopo
+ * @version 2.0
+ *
+ */
 public class Fattoriale {
+	/**
+	 * Costruttore della classe Fattoriale 
+	 */
 	public Fattoriale() {
 	}
 
 	public float fattoriale(int n) {
 		int fatt, i;
 		fatt = 1;
-		for (i = 1; i < n; i++) {
+		for (i = 1; i < n; i++) { //<=
 			fatt = fatt * i;
 		}
 		return fatt;
